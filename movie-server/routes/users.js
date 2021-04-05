324dfe59505d6f07e3a1377939d4244053ab9a5e
sukanya934 <sukanya.chakraborty@codeclouds.in>
2021-04-05 14:41:41 +0530

    user registration

--- movie-server/routes/users.js
+++ movie-server/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var usercontroller=require('../controller/UserController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.status(200).json({ result : 'user...' })
 });
+
+router.post('/register',usercontroller.register_user);
+
+
+
 
 module.exports = router;
