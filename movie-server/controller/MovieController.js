437fd4fea1abb0c44d2c0fdf2b479511ee59bd44
Avijit Mondal <avijit.mondal@codeclouds.in>
2021-04-06 17:36:52 +0530

    movie add,edit,list,single view

--- movie-server/controller/MovieController.js
+++ movie-server/controller/MovieController.js
@@ -8,37 +8,68 @@ const Movie = require('../models/Movie');
 const bcrypt = require('bcrypt');
 const { check, validationResult, body } = require('express-validator');
 
-exports.validate = (method) => {
-    switch (method) {
-      case 'createUser': {
-       return [ 
-            body('name', 'Name is required').notEmpty(),
-            body('email', 'Invalid email').exists().isEmail(),
-            body('phone').optional().isInt(),
-            body('status').optional().isIn(['enabled', 'disabled'])
-         ]   
-      }
-    }
-}
-
 exports.createMovie = async(req,res,next) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            res.status(200).json({ errors: errors.array() });
+            return res.status(status.OK).json({ errors: errors.array() });
         }
-        // const { title, genre, stock, rate } = req.body;
-        // movie = new Movie({
-        //     title,
-        //     genre,
-        //     stock,
-        //     rate
-        // });
-        // await movie.save();
-        // res.status(200).json({ success: true, result: 'successfully genre saved' })
-        console.log(req.body)
-        res.status(200).json({ success: true, result: req.body })
+        const { title, genre, stock, rate } = req.body;
+        movie = new Movie({
+            title,
+            genre,
+            stock,
+            rate
+        });
+        await movie.save();
+        return res.status(status.OK).json({ success: true, result: 'successfully movie saved' })
     } catch (err) {
-        res.status(500).json({ success: false,result: err })
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
+    }
+}
+
+exports.getSingleMovie = async(req,res,next) => {
+    try {
+        let movie = await Movie.findById(req.params.movie_id)
+        if(!movie){
+            return res.status(status.NOT_FOUND).json({ success : false, msg : 'invalid movie' })
+        }
+        return res.status(status.OK).json({ success: true, result: movie })
+    } catch (err) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
+    }
+}
+
+exports.updateMovie = async(req,res,next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(status.OK).json({ errors: errors.array() });
+        }
+        const { title, genre, stock, rate } = req.body;
+
+        let movie = await Movie.findById(req.params.movie_id)
+        if(!movie){
+            return res.status(status.NOT_FOUND).json({ success : false, msg : 'invalid movie' })
+        }
+        const payload = {
+            title,
+            genre,
+            stock,
+            rate
+        }
+        await Movie.findByIdAndUpdate(req.params.movie_id, payload);
+        return res.status(status.OK).json({ success: true, msg: 'movie updated' })
+    } catch (err) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
+    }
+}
+
+exports.movieList = async(req,res,next) => {
+    try {
+        let movie = await Movie.find().populate('genre')
+        return res.status(status.OK).json({ success: true, msg: 'movie list', result: movie })
+    } catch (err) {
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
     }
 }
