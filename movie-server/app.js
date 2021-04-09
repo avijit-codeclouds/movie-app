155bc530e9d91af7e0c08ca1a86062286bbf3b25
Avijit Mondal <avijit.mondal@codeclouds.in>
2021-04-09 15:54:37 +0530

    rent apis

--- movie-server/app.js
+++ movie-server/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  //res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- movie-server/controller/RentController.js
+++ movie-server/controller/RentController.js
@@ -9,11 +9,40 @@ const Rent = require('../models/Rent')
 const User = require('../models/User')
 const bcrypt = require('bcrypt');
 const { check, validationResult, body } = require('express-validator');
-const moment = require('moment')
+const moment = require('moment');
+
+exports.rentDelete = async(req,res,next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(status.OK).json({ errors: errors.array() });
+        }
+        const { user, movie } = req.body;
+        
+        const getUser = await Rent.findOne({ user: user })
+        
+        if(!getUser){
+            return res.status(status.OK).json({ success : false, msg : 'no one movie is found with this user' })
+        }
+        const updateRentlist = await Rent.findById(getUser._id)
+        const anyMovieExists = updateRentlist.movies.filter(e => e.movie.toString() === movie);
+        if(anyMovieExists.length <= 0){
+            return res.status(status.OK).json({ success : false, msg : 'invalid movie' })
+        }else{
+            updateRentlist.movies = updateRentlist.movies.filter(e => e.movie.toString() !== movie);
+            await updateRentlist.save();
+            return res.status(status.OK).json({ success: true, result: updateRentlist, msg: 'rent list updated' })
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
+    }
+}
 
 exports.rentList = async(req,res,next) => {
     try {
-        const rent = await Rent.find().populate(['movies.movie']).populate('user').select('-password')
+        const rent = await Rent.find().populate(['movies.movie']).
+        populate({ path: 'user', select: '-password' })
         return res.status(status.OK).json({ success: true, msg: 'rented movie list', result: rent })
     } catch (err) {
         return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
@@ -33,7 +62,7 @@ exports.rentMovie = async(req,res,next) => {
         }
 
         const currentTime = moment().format('MMMM Do YYYY, hh:mm A')
-        const expireTime = moment().add(2, 'hours').format('MMMM Do YYYY, hh:mm A');
+        const expireTime = moment().add(48, 'hours').format('MMMM Do YYYY, hh:mm A');
         
         const getUser = await Rent.findOne({ user: user })
         const payload = {
@@ -52,13 +81,13 @@ exports.rentMovie = async(req,res,next) => {
             if (getUser.movies.some((movies) => movies.movie.toString() === movie )) {
                 return res.status(status.OK).json({ success: false, result: 'movie already on rent' });
             }else{
-                rent.movies.unshift(payload)
-                const movieList = await rent.save();
+                getUser.movies.unshift(payload)
+                const movieList = await getUser.save();
                 return res.status(status.OK).json({ success: true, result: 'successfully rent saved',result: movieList })
             }
         }
     } catch (err) {
-        // console.log(err)
+        console.log(err)
         return res.status(status.INTERNAL_SERVER_ERROR).json({ success: false, result: err })
     }
 }
