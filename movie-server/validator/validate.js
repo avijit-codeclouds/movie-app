6937b175580dbf1a8ecf7c5f0d64f9decf9d4299
Sayan Chakraborty <sayan.chakraborty@codeclouds.in>
2021-04-09 16:40:44 +0530

    Renamed validator

---
+++ movie-server/validator/validate.js
@@ -0,0 +1,30 @@
+const {
+   check,
+   validationResult,
+   body
+} = require('express-validator');
+
+exports.validate = (method) => {
+   switch (method) {
+      case 'createMovie': {
+         return [
+            body('title', 'Title is required').notEmpty(),
+            body('genre', 'Genre is required').notEmpty(),
+            body('stock', 'Stock is required').notEmpty(),
+            body('rate', 'Rate is required').notEmpty(),
+         ];
+      }
+      case 'wishList': {
+         return [
+            body('user', 'User is required').notEmpty(),
+            body('movie', 'Movie is required').notEmpty(),
+         ];
+      }
+      case 'customer': {
+         return [
+            body('name', 'Name is required').notEmpty(),
+            body('details', 'Details is required').notEmpty(),
+         ];
+      }
+   }
+};
