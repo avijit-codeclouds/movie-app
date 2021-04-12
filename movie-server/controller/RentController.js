f93921081984104be2ff45231e875614e9ea7476
Avijit Mondal <avijit.mondal@codeclouds.in>
2021-04-12 15:59:34 +0530

    response changed

--- movie-server/controller/RentController.js
+++ movie-server/controller/RentController.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const status = require('http-status');
-const { data, cssNumber } = require('jquery');
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
 const Movie = require('../models/Movie');
 const Rent = require('../models/Rent')
 const User = require('../models/User')
-const bcrypt = require('bcrypt');
 const { check, validationResult, body } = require('express-validator');
 const moment = require('moment');
-const _ = require('lodash');
 const { response } = require('../helper/helper');
 
 
@@ -18,27 +12,26 @@ exports.cancelRentMovie = async(req,res,next) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(status.OK).json({ errors: errors.array() });
+            const msg = 'validation error'
+            return res.status(status.OK).json(response(false,errors.array(),msg));
         }
         const { user, movie } = req.body;
         
         const getUser = await Rent.findOne({ user: user })
         if(!getUser){
-            return res.status(status.OK).json({ success : false, msg : 'no one movie is found with this user' })
+            return res.status(status.OK).json(response(false,'','no one movie is found with this user'))
         }
         const updateRentlist = await Rent.findById(getUser._id)
         const anyMovieExists = updateRentlist.movies.filter(e => e.movie.toString() === movie);
         if(anyMovieExists.length <= 0){
-            return res.status(status.OK).json({ success : false, msg : 'invalid movie' })
+            return res.status(status.OK).json(response(false,'','invalid movie'))
         }else{
             const newPayload = anyMovieExists[0]
             newPayload.canceled = true
             updateRentlist.movies = updateRentlist.movies.filter(e => e.movie.toString() !== movie);
             updateRentlist.movies.unshift(newPayload)
             await updateRentlist.save();
-            return res.status(status.OK).json({ 
-                success: true, result: updateRentlist, msg: 'rent canceled' 
-            })
+            return res.status(status.OK).json(response(true,updateRentlist,'rent canceled' ))
         }
     } catch (err) {
         console.log(err)
@@ -50,68 +43,29 @@ exports.pauseRentMovie = async(req,res,next) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(status.OK).json({ errors: errors.array() });
+            const msg = 'validation error'
+            return res.status(status.OK).json(response(false,errors.array(),msg));
         }
         const { user, movie } = req.body;
         
         const getUser = await Rent.findOne({ user: user })
         if(!getUser){
-            return res.status(status.OK).json({ success : false, msg : 'no one movie is found with this user' })
+            return res.status(status.OK).json(response(false,'','no one movie is found with this user'))
         }
         const updateRentlist = await Rent.findById(getUser._id)
         const anyMovieExists = updateRentlist.movies.filter(e => e.movie.toString() === movie);
         if(anyMovieExists.length <= 0){
-            return res.status(status.OK).json({ success : false, msg : 'invalid movie' })
+            return res.status(status.OK).json(response(false,'','invalid movie'))
         }else{
             const newPayload = anyMovieExists[0]
             newPayload.paused = true
             updateRentlist.movies = updateRentlist.movies.filter(e => e.movie.toString() !== movie);
             updateRentlist.movies.unshift(newPayload)
             await updateRentlist.save();
-            return res.status(status.OK).json({ 
-                success: true, result: updateRentlist, msg: 'rent paused' 
-            })
+            return res.status(status.OK).json(response(true,updateRentlist,'rent paused' ))
         }
     } catch (err) {
         console.log(err)
-        return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
-    }
-}
-
-exports.rentDelete = async(req,res,next) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(status.OK).json({ errors: errors.array() });
-        }
-        const { user, movie } = req.body;
-        
-        const getUser = await Rent.findOne({ user: user })
-        
-        if(!getUser){
-            return res.status(status.OK).json({ success : false, msg : 'no one movie is found with this user' })
-        }
-        const updateRentlist = await Rent.findById(getUser._id)
-        const anyMovieExists = updateRentlist.movies.filter(e => e.movie.toString() === movie);
-        if(anyMovieExists.length <= 0){
-            return res.status(status.OK).json({ success : false, msg : 'invalid movie' })
-        }else{
-            updateRentlist.movies = updateRentlist.movies.filter(e => e.movie.toString() !== movie);
-            await updateRentlist.save();
-            return res.status(status.OK).json({ success: true, result: updateRentlist, msg: 'rent list updated' })
-        }
-    } catch (err) {
-        console.log(err)
-        return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
-    }
-}
-
-exports.rentList = async(req,res,next) => {
-    try {
-        const rent = await Rent.find().populate(['movies.movie']).
-        populate({ path: 'user', select: '-password' })
-        return res.status(status.OK).json({ success: true, msg: 'rented movie list', result: rent })
-    } catch (err) {
         return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
     }
 }
@@ -120,12 +74,13 @@ exports.rentMovie = async(req,res,next) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(status.OK).json({ errors: errors.array() });
+            const msg = 'validation error'
+            return res.status(status.OK).json(response(false,errors.array(),msg));
         }
         const { user, movie } = req.body;
         let getMovie = await Movie.findById(movie)
         if(!getMovie){
-            return res.status(status.OK).json({ success : false, msg : 'invalid movie' })
+            return res.status(status.OK).json(response(false,'','invalid movie'))
         }
 
         const currentTime = moment().format('MMMM Do YYYY, hh:mm A')
@@ -141,19 +96,58 @@ exports.rentMovie = async(req,res,next) => {
                 movies : payload
             });
             const newRent = await rent.save();
-            return res.status(status.OK).json({ success: true, result: 'successfully rent saved',result: newRent })
+            return res.status(status.OK).json(response(true,newRent,'successfully rent saved'))
         }else{
             // console.log(getUser.movies)
             if (getUser.movies.some((movies) => movies.movie.toString() === movie )) {
-                return res.status(status.OK).json({ success: false, result: 'movie already on rent' });
+                return res.status(status.OK).json(response(false,'','movie already on rent'))
             }else{
                 getUser.movies.unshift(payload)
                 const movieList = await getUser.save();
-                return res.status(status.OK).json({ success: true, result: 'successfully rent saved',result: movieList })
+                return res.status(status.OK).json(response(true,movieList,'successfully rent saved'))
             }
         }
     } catch (err) {
         console.log(err)
+        return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
+    }
+}
+
+exports.rentDelete = async(req,res,next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const msg = 'validation error'
+            return res.status(status.OK).json(response(false,errors.array(),msg));
+        }
+        const { user, movie } = req.body;
+        
+        const getUser = await Rent.findOne({ user: user })
+        
+        if(!getUser){
+            return res.status(status.OK).json(response(false,'','no one movie is found with this user'))
+        }
+        const updateRentlist = await Rent.findById(getUser._id)
+        const anyMovieExists = updateRentlist.movies.filter(e => e.movie.toString() === movie);
+        if(anyMovieExists.length <= 0){
+            return res.status(status.OK).json(response(false,'','invalid movie'))
+        }else{
+            updateRentlist.movies = updateRentlist.movies.filter(e => e.movie.toString() !== movie);
+            await updateRentlist.save();
+            return res.status(status.OK).json(response(true,updateRentlist,'rent list updated'))
+        }
+    } catch (err) {
+        console.log(err)
+        return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
+    }
+}
+
+exports.rentList = async(req,res,next) => {
+    try {
+        const rent = await Rent.find().populate(['movies.movie']).
+        populate({ path: 'user', select: '-password' })
+        return res.status(status.OK).json(response(true,rent,'rented movie list'))
+    } catch (err) {
         return res.status(status.INTERNAL_SERVER_ERROR).json(response(false,err))
     }
 }
