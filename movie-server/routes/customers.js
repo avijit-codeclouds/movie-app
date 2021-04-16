4203a2a811621bc67ac4bbcac032ba958c25e041
Sayan Chakraborty <sayan.chakraborty@codeclouds.in>
2021-04-16 16:30:16 +0530

    Change customer logic

--- movie-server/routes/customers.js
+++ movie-server/routes/customers.js
@@ -1,18 +1,14 @@
 const express               = require('express');
 const router                = express.Router();
 const customerController    = require('../controller/CustomerController.js');
-const validator             = require('../validator/validate');
-const helper                = require('../helper/helper');
 
 router
     .route('/')
-    .get(customerController.get_all_customers)
-    .post(validator.validate('customer') , helper.handle_validation_error ,customerController.create_customer);
+    .get(customerController.get_all_customers);
 
 router
     .route('/:id')
     .get(customerController.get_customer)
-    .patch(validator.validate('customer'), helper.handle_validation_error ,customerController.edit_customer)
     .delete(customerController.delete_customer);
 
 router
