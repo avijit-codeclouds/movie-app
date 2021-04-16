ed76c4778e5292045fd398b1e714f64148d9996d
Sayan Chakraborty <sayan.chakraborty@codeclouds.in>
2021-04-16 12:32:25 +0530

    Dev: Movie controller refactor

--- movie-server/controller/MovieController.js
+++ movie-server/controller/MovieController.js
@@ -1,122 +1,137 @@
 require("dotenv").config();
-const status = require("http-status");
-const Movie = require("../models/Movie");
-const APIFeatuers = require("../utils/apiFeatures");
-const { response } = require("../helper/helper");
+const status 		= require("http-status");
+const Movie 		= require("../models/Movie");
+const APIFeatuers 	= require("../utils/apiFeatures");
+const { response } 	= require("../helper/helper");
 
 exports.create_movie = async (req, res) => {
-  try {
-    const { title, genre, stock, rate } = req.body;
+	try {
+		const { title, genre, stock, rate } = req.body;
 
-    movie = new Movie({
-      title,
-      genre,
-      stock,
-      rate,
-    });
+		movie = new Movie({
+			title,
+			genre,
+			stock,
+			rate,
+		});
 
-    const newMovie = await movie.save();
-    const msg = "Movie Successfully saved";
+		const newMovie = await movie.save();
+		const msg = "Movie Successfully saved";
 
-    return res.status(status.OK).json(response(true, newMovie, msg));
-  } catch (err) {
-    return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
-  }
+		return res.status(status.CREATED).json(response(true, newMovie, msg));
+
+	}
+	catch (err)
+	{
+		return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
+	}
 };
 
-exports.view_movie = async (req, res, next) => {
-  try {
-    let movie = await Movie.findById(req.params.movie_id);
+exports.view_movie = async (req, res) => {
+	try
+	{
+		let movie = await Movie.findById(req.params.movie_id);
 
-    const invalidMsg = "invalid movie";
-    if (!movie) {
-      return res
-        .status(status.NOT_FOUND)
-        .json(response(false, movie, invalidMsg));
-    }
+		const invalidMsg = "Invalid Movie";
+		if (!movie)
+		{
+			return res.status(status.OK).json(response(false, movie, invalidMsg));
+		}
 
-    const msg = "here is your movie";
+		const msg = "Here is your movie";
 
-    return res.status(status.OK).json(response(true, movie, msg));
-  } catch (err) {
-    return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
-  }
+		return res.status(status.OK).json(response(true, movie, msg));
+	}
+	catch (err)
+	{
+		return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
+	}
 };
 
-exports.update_movie = async (req, res, next) => {
-  try {
-    const { title, genre, stock, rate } = req.body;
+exports.update_movie = async (req, res) => {
+	try
+	{
+		const { title, genre, stock, rate } = req.body;
 
-    let movie = await Movie.findById(req.params.movie_id);
+		let movie = await Movie.findById(req.params.movie_id);
 
-    if (!movie) {
-      const invalidMsg = "invalid movie";
-      return res
-        .status(status.NOT_FOUND)
-        .json(response(false, movie, invalidMsg));
-    }
+		if (!movie)
+		{
+			const invalidMsg = "Invalid Movie";
+			return res.status(status.NOT_FOUND).json(response(false, movie, invalidMsg));
+		}
 
-    const payload = {
-      title,
-      genre,
-      stock,
-      rate,
-    };
+		const payload = {
+			title,
+			genre,
+			stock,
+			rate,
+		};
 
-    const updateMovie = await Movie.findByIdAndUpdate(
-      req.params.movie_id,
-      payload,
-      {
-        new: true,
-      }
-    );
+		const updateMovie = await Movie.findByIdAndUpdate(
+			req.params.movie_id,
+			payload,
+			{
+			new: true,
+			}
+		);
 
-    const msg = "movie updated";
+		const msg = "Movie Updated";
 
-    return res.status(status.OK).json(response(true, updateMovie, msg));
-  } catch (err) {
-    return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
-  }
+		return res.status(status.OK).json(response(true, updateMovie, msg));
+
+	}
+	catch (err)
+	{
+		return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
+	}
 };
 
-exports.movie_list = async (req, res, next) => {
-  try {
-    // let movie = await Movie.find().populate('genre')
-    const msg = "movie list";
-    const features = new APIFeatuers(
-      Movie.find().populate("genre"),
-      req.query
-    )
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
+exports.movie_list = async (req, res) => {
+	try
+	{
 
-    const movie = await features.query;
-    return res.status(status.OK).json(response(true, movie, msg));
-  } catch (err) {
-    return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
-  }
+		const msg = "movie list";
+		const features = new APIFeatuers(
+							Movie.find().populate("genre"),
+							req.query
+						)
+						.filter()
+						.sort()
+						.limitFields()
+						.paginate();
+
+		const movie = await features.query;
+
+		return res.status(status.OK).json(response(true, movie, msg));
+	}
+	catch (err)
+	{
+		return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
+	}
 };
 
-exports.delete_movie = async (req, res, next) => {
-  try {
-    let movie = await Movie.findById(req.params.movie_id);
+exports.delete_movie = async (req, res) => {
+	try
+	{
+		let movie = await Movie.findById(req.params.movie_id);
 
-    if (!movie) {
-      const invalidMsg = "Invalid Movie";
-      return res
-        .status(status.NOT_FOUND)
-        .json(response(false, movie, invalidMsg));
-    }
-   
+		if (!movie)
+		{
+			const invalidMsg = "Invalid Movie";
 
-      const getMovie = await Movie.findByIdAndDelete(req.params.movie_id);
+			return res.status(status.NOT_FOUND).json(response(false, movie, invalidMsg));
+		}
 
-      const msg = "Movie Deleted";
-      return res.status(status.OK).json(response(true, msg));
+		const getMovie = await Movie.findByIdAndDelete(req.params.movie_id);
 
-  } catch (err) {
-    return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
-  }
+		const msg = "Movie Deleted";
+
+		return res.status(status.NO_CONTENT).json(response(true));
+
+	}
+	catch (err)
+	{
+		return res.status(status.INTERNAL_SERVER_ERROR).json(response(false, err));
+	}
 };
