9f4c05368a8928e0b1be66e83cc6bcafe31b2a11
Sayan Chakraborty <sayan.chakraborty@codeclouds.in>
2021-04-16 11:23:49 +0530

    Dev: REfactor movie routes

--- movie-server/routes/movie.js
+++ movie-server/routes/movie.js
@@ -4,15 +4,16 @@ const movieController   = require('../controller/MovieController');
 const movieValidator    = require('../validator/validate');
 const { handle_validation_error }  = require('../helper/helper');
 
-router.post('/add', movieValidator.validate('createMovie'),handle_validation_error,movieController.create_movie);
+router
+    .route('/')
+    .get(movieController.movie_list)
+    .post(movieValidator.validate('createMovie'),handle_validation_error,movieController.create_movie);
 
-router.post('/edit/:movie_id',movieValidator.validate('createMovie'),handle_validation_error,movieController.update_movie);
-
-router.get('/list',movieController.movie_list);
-
-router.post('/delete/:movie_id',movieController.delete_movie);
-
-router.get('/:movie_id',movieController.view_movie);
+router
+    .route('/:movie_id')
+    .get(movieController.view_movie)
+    .patch(movieValidator.validate('createMovie'),handle_validation_error,movieController.update_movie)
+    .delete(movieController.delete_movie);
 
 
 module.exports = router;
