bfe5d83e94f290407426eabb4c70c5363573f9cd
avijit-codeclouds <80821537+avijit-codeclouds@users.noreply.github.com>
2021-04-19 17:19:43 +0530

    Revert "Expiration cron job added"

--- movie-server/app.js
+++ movie-server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const createError   = require('http-errors');
-const bodyParser    =  require('body-parser');
+const bodyParser    = require('body-parser');
 const passport      = require('passport');
 
 const express       = require('express');
@@ -9,25 +9,29 @@ const path          = require('path');
 const cookieParser  = require('cookie-parser');
 const morgan        = require('morgan');
 const winston       = require('./config/winston');
-const cors          = require('cors');
 const helmet        = require('helmet');
 const routes        = require('./routes');
 const app           = express();
-const cron = require('node-cron');
-const { expire_rent_movie }  = require('./jobs/Rentjob');
-const { modifyExpire }  = require('./jobs/rendModifications');
+const cron          = require('node-cron')
+const { expire_rent_movie }  = require('./jobs/Rentjob')
 
 
 
 
 app.use(morgan('combined', { stream: winston.stream }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('cors')())
 app.use(helmet());
-app.disable('x-powered-by');
 app.use(cookieParser());
+app.use(require('compression')())
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
+
+cron.schedule('* * * * *', () => {
+  expire_rent_movie()
+});
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -35,12 +39,28 @@ app.use(function (req, res, next) {
   next();
 });
 
-cron.schedule('* * * * *', () => {
-  expire_rent_movie();
-  modifyExpire();
-});
 
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use('*',(req, res, next) => {
+  next(createError(404));
+});
+
+
+// error handler
+app.use((err, req, res, next) => {
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    error : {
+      message : err.message
+    }
+  })
+});
+
+
+
 
 module.exports = app;
 
--- movie-server/models/Rent.js
+++ movie-server/models/Rent.js
@@ -2,67 +2,52 @@ const mongoose = require('mongoose');
 const moment = require('moment');
 
 const RentSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  movies: [{
-    movie: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Movie',
-      required: true
+    user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+          required:true,
+          index: true
     },
-    startday: {
-      type: String,
-      required: true,
-      default: moment().format('MMMM Do YYYY, hh:mm A')
-    },
-    endday: {
-      type: String,
-      required: true,
-      default: moment().add(48, 'hours').format('MMMM Do YYYY, hh:mm A')
-    },
-    expired: {
+    movies: [
+      {
+        movie: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Movie',
+          required:true
+        },
+        startday: {
+            type: String,
+            required:true,
+            default : moment().format('MMMM Do YYYY, hh:mm A')
+        },
+        endday: {
+            type: String,
+            required:true,
+            default : moment().add(48, 'hours').format('MMMM Do YYYY, hh:mm A')
+        },
+        expired: {
+            type: Boolean,
+            default: false
+        },
+        paused: {
+          type: Boolean,
+          default: false
+        },
+        canceled: {
+          type: Boolean,
+          default: false
+        },
+        createdAt: { type: String, default: moment().format("YYYY-MM-DD[T]HH:mm") },
+        expireAt: { type: String, index: true, default: moment().add(48, 'hours').format("YYYY-MM-DD[T]HH:mm") }
+      }
+    ],
+    isLocked: {
       type: Boolean,
       default: false
-    },
-    paused: {
-      type: Boolean,
-      default: false
-    },
-    canceled: {
-      type: Boolean,
-      default: false
-    },
-    createdAt: {
-      type: String,
-      default: moment().format("YYYY-MM-DD[T]HH:mm")
-    },
-    expireAt: {
-      type: String,
-      index: true,
-      default: moment().add(48, 'hours').format("YYYY-MM-DD[T]HH:mm")
     }
-  }],
-  isLocked: {
-    type: Boolean,
-    default: false
-  }
-}, {
-  timestamps: true
-});
-
-RentSchema.methods.fixExpired = async function (rentId) {
-  const RentMovieIndex = this.movies.findIndex(cp => {
-    return cp._id.toString() === rentId.toString();
+  },
+  {
+      timestamps: true
   });
-  if (RentMovieIndex >= 0) {
-    this.movies[RentMovieIndex].expired = true;
-  }
-  return new Promise((resolve, reject) => {
-    resolve(this.save());
-  });
-};
 
 module.exports = mongoose.model('Rent', RentSchema);
