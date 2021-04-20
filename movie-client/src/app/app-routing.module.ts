a7244d4693d8a00e8466f01ff10a8b8d4a1b2c19
CISPLIO308 <68638776+CISPLIO308@users.noreply.github.com>
2021-04-20 17:34:26 +0530

    Dynamic title (#134)
    * Dev - added data to routes
    
    * Dev - set title

--- movie-client/src/app/app-routing.module.ts
+++ movie-client/src/app/app-routing.module.ts
@@ -13,18 +13,83 @@ import { WishlistComponent } from './pages/wishlist/wishlist.component';
 
 const routes: Routes = [
   // { path: '', component: HomeComponent },
-  { path: '', loadChildren: './pages/movies/movies.module#MoviesModule'},
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'customers', component: CustomersComponent, canActivate: [AuthGuard] },
-  { path: 'rentals', component: RentalsComponent, canActivate: [AuthGuard] },
-  { path: 'rentals/:user_id', component: RentalsComponent, canActivate: [AuthGuard] },
-  { path: 'movies/new', component: NewmovieComponent, canActivate: [AuthGuard] },
-  { path: 'movies/:movie_id',component: UpdatemovieComponent, canActivate: [AuthGuard] },
-  { path: 'wishlist', component: WishlistComponent,canActivate: [AuthGuard] },
-  { path: 'logout', component: LogoutComponent },
-  { path: '404', component: NotfoundComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { 
+    path: '', 
+    loadChildren: './pages/movies/movies.module#MoviesModule',
+    data: {
+      title: 'Movies ::Movie-App'
+    }
+  }, { 
+    path: 'register', 
+    component: RegisterComponent, 
+    data: {
+      title: 'Register ::Movie-App'
+    }
+  }, { 
+    path: 'login', 
+    component: LoginComponent, 
+    data: {
+      title: 'Login ::Movie-App'
+    }
+  }, { 
+    path: 'customers', 
+    component: CustomersComponent, 
+    canActivate: [AuthGuard], 
+    data: {
+      title: 'Customers ::Movie-App'
+    }
+  }, { 
+    path: 'rentals', 
+    component: RentalsComponent, 
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Rentals ::Movie-App'
+    }
+  }, { 
+    path: 'rentals/:user_id', 
+    component: RentalsComponent, 
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Rental Details ::Movie-App'
+    }
+  }, { 
+    path: 'movies/new', 
+    component: NewmovieComponent, 
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Add New Movie ::Movie-App'
+    }
+  }, { 
+    path: 'movies/:movie_id',
+    component: UpdatemovieComponent, 
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Edit Movie ::Movie-App'
+    }
+  }, { 
+    path: 'wishlist', 
+    component: WishlistComponent,
+    canActivate: [AuthGuard],
+    data: {
+      title: 'Wishlist ::Movie-App'
+    }
+  }, { 
+    path: 'logout', 
+    component: LogoutComponent,
+    data: {
+      title: 'Logout ::Movie-App'
+    }
+  }, { 
+    path: '404', 
+    component: NotfoundComponent,
+    data: {
+      title: 'Page Not Found ::Movie-App'
+    }
+  }, { 
+    path: '**', 
+    redirectTo: '', 
+    pathMatch: 'full' 
+  }
 ];
 
 @NgModule({
