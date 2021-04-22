9c5b838e7a0bb65f9921159607b823b321753a12
CISPLIO308 <68638776+CISPLIO308@users.noreply.github.com>
2021-04-22 18:08:42 +0530

    Dev - code refactoring (#173)

--- movie-client/src/app/pages/movies/movies.component.ts
+++ movie-client/src/app/pages/movies/movies.component.ts
@@ -1,44 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { MovieService } from "./../../services/movie.service";
 import { GenerService } from "./../../services/gener.service";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { AuthService } from "./../../services/auth.service";
-import { ActivatedRoute, Router } from "@angular/router";
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { MatSnackBar } from '@angular/material/snack-bar'; 
+import { Router } from "@angular/router";
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { finalize } from "rxjs/operators";
-
-declare var $: any;
-
-
 @Component({
   selector: "app-movies",
   templateUrl: "./movies.component.html",
   styleUrls: ["./movies.component.css"],
 })
 export class MoviesComponent implements OnInit {
-  token: boolean;
   movies: any;
   generes: [];
-  query;
   p;
   movie:any;
   storeMovies: [];
-  isShowDivIf = false;
-  status: any;
-  icon: boolean;
-  isActive = true;
-  SelectedIDs: any = [];
-  user_id: any;
-  isChecked: any;
   wishList = [];
-  emptyArr = [];
-  clickeItem: number = 0;
-  user : any
-  getUserId : any
-  showProgress : boolean = false;
+  showProgress: boolean = false;
   selectedGenre: string = 'all';
-  jwtHelper = new JwtHelperService();
   loadingMovies: boolean = false;
   loadingGenres: boolean = false;
 
@@ -47,9 +27,6 @@ export class MoviesComponent implements OnInit {
     data: null,
     working: false
   };
-  isCanceling: boolean = false;
-
-  id: string = 'modal-'+ (Math.random() * 100).toFixed(0);
 
   constructor(
     public movieservice: MovieService,
@@ -57,29 +34,30 @@ export class MoviesComponent implements OnInit {
     public authservice: AuthService,
     public router: Router,
     private _snackBar: MatSnackBar,
-  ) {
-    this.authservice.user.subscribe(x => this.user = x);
-    const decode = this.jwtHelper.decodeToken(this.user);
-    this.getUserId = decode['id'] 
-  }
+  ) { }
 
   ngOnInit() {
-    this.user_id = localStorage.getItem("user_id");
-    if (localStorage.getItem("user") === null) {
-      this.token = true;
-    } else {
-      console.log("Unauthorized");
-    }
     this.getGenere();
-    this.getMovies();
-    this.getWishlist();
+    if (this.authservice.isAuth) {
+      this.loadData();
+    } else {
+      this.authservice.rawUserSubjectInstance.subscribe((user) => {
+        this.loadData();
+      });
+    }
   }
 
-  openSnackBar(message: string, action: string = 'Done') { 
-    // openSnackBar('GAME ONE','HURRAH !!!!!')
-    this._snackBar.open(message, action, { 
-      duration: 2000, 
-    }); 
+  loadData() {
+    this.getMovies();
+    if (this.authservice.isUser) {
+      this.getWishlist();
+    }
+  }
+
+  openSnackBar(message: string, action: string = 'Done') {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
   }
 
   getGenre(genreType) {
@@ -112,6 +90,7 @@ export class MoviesComponent implements OnInit {
       this.storeMovies = this.movies;
     });
   }
+  
   getGenere() {
     this.loadingGenres = true;
     this.genereservice.generList().pipe(finalize(() => {
@@ -120,12 +99,14 @@ export class MoviesComponent implements OnInit {
       this.generes = data["result"];
     });
   }
+
   getWishlist() {
     this.movieservice.getMovieWishlist().subscribe((data) => {
       this.wishList = data["result"];
       //  console.log( this.wishList);
     });
   }
+
   getStatus(_id) {
     if (!_id) {
       return false;
@@ -134,7 +115,7 @@ export class MoviesComponent implements OnInit {
       this.wishList[0].movies.map((e) => {
         if (e._id == _id) stat++;
       });
-      
+
       if (stat > 0) {
         return true;
       } else {
@@ -143,84 +124,50 @@ export class MoviesComponent implements OnInit {
     }
   }
 
-  // selectID(_id, event) {
-  //   let data = {
-  //     user: this.user_id,
-  //     movies: [_id],
-  //   };
-  //   window.location.reload();
-  //   this.movieservice.movieWishlist(data).subscribe((data) => {
-  //     console.log(data);
-  //     window.location.reload();
-  //     // data.data.movies.forEach(chk => {
-  //     //   console.log(chk.isChecked)
-  //   });
-  //   //   // this.isChecked=data.data.movies[0].isChecked
-  //   //   // console.log();
-  //   //    this.ngOnInit();
-  //   // })
-  // }
-
-  rentMovie(movie_id){
-    console.log('clicked')
-    console.log(movie_id)
+  rentMovie(movie_id) {
     let payload = {
-      movie : movie_id,
-      user : this.getUserId
+      movie: movie_id,
+      user: this.authservice.me._id
     }
     this.showProgress = true
     this.movieservice.rentMovies(payload).subscribe(res => {
-      console.log(res)
       this.showProgress = false
-      if(res.success == true){
+      if (res.success == true) {
         this.openSnackBar('Successfully you have subscribed')
-      }else{
+      } else {
         this.openSnackBar(res.message)
       }
-    },err => {
+    }, err => {
       console.log(err)
     })
   }
 
   async wishListClicked(itemId: any, event) {
-    this.clickeItem++;
-    console.log("clicked");
-    console.log(`clickeItem :: ${this.clickeItem}`);
-    // this.buttonClicked.next(itemId);
     let data = {
       movies: itemId,
       checked: event.target.checked,
     };
-    console.log(data);
-    this.showProgress = true
-    // Promise.all([
-    //   this.movieservice.fetchData(data)
-    // ]).then(res => {
-    //   console.log(res)
-    //   console.log('all done')
-    //   console.log(this.clickeItem)
-    // });
+
+    this.showProgress = true;
     const res = await this.movieservice.fetchData(data);
     this.showProgress = false
     this.openSnackBar('Successfully Updated Movie Wishlist')
-    // this.getMovies();
-    // this.getWishlist();
-    this.ngOnInit();
+    this.getWishlist();
   }
 
-  deleteMovie(_id : any) {
-      this.movieservice.deleteMovie(_id).pipe(finalize(() => {
-        this.deleteRestoreModalConfig.working = false;
-      })).subscribe((response) => {          
-        if (response.success) {
-          this.deleteRestoreModalConfig.show = false;
-          this.openSnackBar("Successfully deleted movie");
-          this.getMovies();
-        }
-      });
+  deleteMovie(_id: any) {
+    this.movieservice.deleteMovie(_id).pipe(finalize(() => {
+      this.deleteRestoreModalConfig.working = false;
+    })).subscribe((response) => {
+      if (response.success) {
+        this.deleteRestoreModalConfig.show = false;
+        this.openSnackBar("Successfully deleted movie");
+        this.getMovies();
+      }
+    });
   }
 
-  deleteOrRestoreMovie(movie, confirm=false) {
+  deleteOrRestoreMovie(movie, confirm = false) {
     if (!confirm) {
       this.deleteRestoreModalConfig.data = {
         movie: movie,
@@ -240,7 +187,7 @@ export class MoviesComponent implements OnInit {
   restoreMovie(_id) {
     this.movieservice.restoreMovie(_id).pipe(finalize(() => {
       this.deleteRestoreModalConfig.working = false;
-    })).subscribe((response) => {          
+    })).subscribe((response) => {
       if (response.success) {
         this.deleteRestoreModalConfig.show = false;
         this.openSnackBar("Successfully restored movie");
