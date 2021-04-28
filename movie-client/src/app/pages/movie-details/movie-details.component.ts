9ad026a5cc1a1b1deff00ef6f3da96416fb6bf0a
Shantanu Terang <shantanu.terang@codeclouds.in>
2021-04-28 12:39:01 +0530

    Dev - added comment form

--- movie-client/src/app/pages/movie-details/movie-details.component.ts
+++ movie-client/src/app/pages/movie-details/movie-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MovieService } from './../../services/movie.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { finalize } from 'rxjs/operators';
+import { ReviewService } from './../../services/review.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -13,17 +14,25 @@ export class MovieDetailsComponent implements OnInit {
   movieId = null;
   loadingMovie: boolean = false;
   movie = null;
+  reviewText: string = "";
+  reviews: Array<any> = [];
+
+  reviewConfig = {
+    showForm: false,
+  };
 
   constructor(
     private movieService: MovieService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private reviewService: ReviewService
   ) { }
 
   ngOnInit() {
     this.movieId = this.route.snapshot.params.id || null;
 
     this.loadMovie(this.movieId);
+    this.loadReviews(this.movieId);
   }
 
   loadMovie(movieId) {
@@ -45,6 +54,38 @@ export class MovieDetailsComponent implements OnInit {
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(url);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
+
+  loadReviews(movieId) {
+    this.reviewService.getReviews(movieId).pipe(finalize(() => {
+
+    })).subscribe((response) => {
+      if (response.success) {
+        this.reviews = response.result;
+      }
+    })
+  }
+
+  toggleReviewForm() {
+    this.reviewConfig.showForm = !this.reviewConfig.showForm;
+  }
+
+  saveReview() {
+    console.log(this.reviewText);
+    let data = {
+      review: this.reviewText,
+      // movieId: this.movieId,
+      isAnonymous: true
+    }
+    this.reviewService.saveReviews(this.movieId, data).pipe(finalize(() => {
+
+    })).subscribe((response) => {
+      if (response.success) {
+        this.reviewText = '';
+        this.toggleReviewForm();
+        this.loadReviews(this.movieId);
+      }
+    })
   }
 
 }
